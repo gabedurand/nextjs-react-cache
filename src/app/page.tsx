87728df3cfe292afd501664a744da19b3c0654ec
mowalky <mowalky@outlook.com>
2023-03-20 22:53:20 -0500

    fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { use } from "react";
+
 export default function Home() {
-  return <main></main>;
+  const data = use(
+    fetch("http://localhost:3000/1.json").then((res) => res.json())
+  );
+
+  return <main>{JSON.stringify(data)}</main>;
 }
